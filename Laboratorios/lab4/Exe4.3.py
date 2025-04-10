89d7c19f5afa340ae4578e242839bb35687ad437
MatheusXavier1 <matheusxavier.20241@poli.ufrj.br>
2025-04-09 23:34:42 -0300

    quadrado mágico

--- Laboratorios/lab4/Exe4.3.py
+++ Laboratorios/lab4/Exe4.3.py
@@ -2,8 +2,12 @@
 
 #Dizemos que uma matriz quadrada de inteiros é um quadrado mágico se a soma dos elementos
 # de cada linha, a soma dos elementos de cada coluna e a soma dos elementos das diagonais principal e secundária são todas iguais.
+[[8,1,6],
+ [3,5,7],
+ [4,2,9]]
 
-a = [[2,3,4],[5,6,4],[5,6,4]]
+a = eval(input())
+
 
 def VerificaLinhas(matriz):
 	somaDeLinhas,a = 0,0
@@ -15,24 +19,37 @@ def VerificaLinhas(matriz):
 			somaDeLinhas += matriz[j][l]
 		flag+=1
 	somaLinhaUnica = somaDeLinhas//len(matriz)
-	return somaLinhaUnica == a
 
-#print(VerificaLinhas(a))
+	return somaLinhaUnica == a
 
 def VerificaColunas(matriz):
 	a,flag,somaDeColunas,somaColunaUnica = 0,0,0,0
 	for j in range(len(matriz)):
 		for l in range(len(matriz[0])):
 			if flag == 0:
-				a += matriz[j][l]
-			somaDeColunas += matriz[j][l]
+				a+=matriz[l][j]
+			somaDeColunas += matriz[l][j]
 		flag+=1
 	somaColunaUnica = somaDeColunas//len(matriz)
 
-	print(somaDeColunas)
-	print(somaColunaUnica)
-	print(a)
-
 	return somaColunaUnica == a
 
+def VerificaDiagonais(matriz):
+	diagonalPrincipal,diagonalSecundaria = 0,0
+	for l in range(len(matriz)):
+		for j in range(len(matriz[0])):
+			if l==j:
+				diagonalPrincipal += matriz[l][j]
+	
+	for l in range(len(matriz)):
+		j = len(matriz[0])
+		while j != 0:
+			j-=1
+			diagonalSecundaria += matriz[l][j]
+			break
+
+	return diagonalPrincipal == diagonalSecundaria
 print(VerificaColunas(a))
+print(VerificaDiagonais(a))
+print(VerificaLinhas(a))
+print(VerificaColunas(a)==VerificaDiagonais(a)==VerificaLinhas(a))

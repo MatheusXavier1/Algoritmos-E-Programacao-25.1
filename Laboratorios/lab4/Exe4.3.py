3e31936a555cd53be96b2153daf824595a4a817a
MatheusXavier1 <matheusxavier.20241@poli.ufrj.br>
2025-04-10 08:25:43 -0300

    recomeco do lab 4.3

--- Laboratorios/lab4/Exe4.3.py
+++ Laboratorios/lab4/Exe4.3.py
@@ -3,36 +3,24 @@
 #Dizemos que uma matriz quadrada de inteiros é um quadrado mágico se a soma dos elementos
 # de cada linha, a soma dos elementos de cada coluna e a soma dos elementos das diagonais principal e secundária são todas iguais.
 
-a = [[2,3,4],[5,6,4],[5,6,4]]
+a =    [[2,3,4],
+	[5,6,4],
+	[5,6,4]]
 
-def VerificaLinhas(matriz):
-	somaDeLinhas,a = 0,0
-	flag = 0
-	for j in  range(len(matriz)):
-		for l in range(len(matriz[0])):
-			if flag == 0:
-				a += matriz[j][l]
-			somaDeLinhas += matriz[j][l]
-		flag+=1
-	somaLinhaUnica = somaDeLinhas//len(matriz)
-	return somaLinhaUnica == a
+def VerificaQuadradoMagico(matriz):
+	valoresLinhas = []
+	for l in range(len(matriz)):
+		somaLinhas = 0
+		for c in range(len(matriz[0])):
+			somaLinhas+= matriz[l][c]
+		valoresLinhas.append(somaLinhas)
+	#verificando se todos os valores sao iguais
+	valoresLinhas.sort()
+	delta = valoresLinhas[-1] - valoresLinhas[0]
 
-#print(VerificaLinhas(a))
+	if delta == 0:
+		print("todos iguais")
+	else:
+		print("tem diferente")
 
-def VerificaColunas(matriz):
-	a,flag,somaDeColunas,somaColunaUnica = 0,0,0,0
-	for j in range(len(matriz)):
-		for l in range(len(matriz[0])):
-			if flag == 0:
-				a += matriz[j][l]
-			somaDeColunas += matriz[j][l]
-		flag+=1
-	somaColunaUnica = somaDeColunas//len(matriz)
-
-	print(somaDeColunas)
-	print(somaColunaUnica)
-	print(a)
-
-	return somaColunaUnica == a
-
-print(VerificaColunas(a))
+print(VerificaQuadradoMagico(a))
